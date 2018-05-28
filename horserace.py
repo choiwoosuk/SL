be42f9067f1cd67315f516ded7ce7ff478617eb7
jarvis <tidk93@naver.com>
2018-05-28 22:32:46 +0900

    add mainframe

--- horserace.py
+++ horserace.py
@@ -3,7 +3,7 @@ from tkinter import font
 
 window=Tk()
 window.title("경주마 추천기")
-canvas=Canvas(window,width=400,height=600)
+canvas=Canvas(window,width=400,height=650)
 canvas.pack()
 
 def mainLogo():
@@ -33,23 +33,15 @@ def rankSelect():
     rankSelectBox.pack()
     rankSelectBox.place(x=40,y=145)
 
-def InitSearchListBox():
-    global SearchListBox
-    ListBoxScrollbar = Scrollbar(window)
-    ListBoxScrollbar.pack()
-    ListBoxScrollbar.place(x=150, y=50)
-
-    TempFont = font.Font(window, size=15, weight='bold', family='Consolas')
-    SearchListBox = Listbox(window, font=TempFont, activestyle='none',
-                            width=10, height=1, borderwidth=12, relief='ridge',
-                            yscrollcommand=ListBoxScrollbar.set)
-
-    SearchListBox.insert(1, "도서관")
-    SearchListBox.insert(2, "모범음식점")
-    SearchListBox.insert(3, "마트")
-    SearchListBox.insert(4, "문화시설")
-    SearchListBox.pack()
-    SearchListBox.place(x=10, y=50)
+def nameLabel():
+    TempFont = font.Font(window, size=10, family = 'HY견고딕')
+    MainText = Label(window,font=TempFont,text="말")
+    MainText.pack()
+    MainText.place(x=90,y=220)
+    
+    MainText = Label(window,font=TempFont,text="기수")
+    MainText.pack()
+    MainText.place(x=280,y=220)
 
 def showHorseRank():
     pass
@@ -58,20 +50,25 @@ def searchLogo():
     TempFont = font.Font(window, size=15, family = 'HY견고딕')
     MainText = Label(window,font=TempFont,text="말 랭킹 검색")
     MainText.pack()
-    MainText.place(x=25,y=565)
+    MainText.place(x=25,y=615)
 
 def searchHorse():
     global inputLabel
     TempFont = font.Font(window, size=12, family='맑은 고딕')
     inputLabel=Entry(window,font=TempFont,width=15,borderwidth=5,relief='ridge')
     inputLabel.pack()
-    inputLabel.place(x=180,y=560)
+    inputLabel.place(x=180,y=610)
 
 def searchButton():
     TempFont=font.Font(window, size=12, weight='bold', family='맑은 고딕')
     SearchButton=Button(window, font=TempFont,text="검색")
     SearchButton.pack()
-    SearchButton.place(x=340,y=557)
+    SearchButton.place(x=340,y=607)
+
+def mainFrame():
+    labelframe=LabelFrame(window,text="랭크",width=350,height=375)
+    labelframe.pack()
+    labelframe.place(x=30,y=200)
 
 mainLogo()
 
@@ -88,7 +85,9 @@ rankSelect()
 searchLogo()
 searchHorse()
 searchButton()
-InitSearchListBox()
+mainFrame()
 
-canvas.create_line(0,550,400,550)
+nameLabel()
+
+canvas.create_line(0,600,400,600)
 window.mainloop()
